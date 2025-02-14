7797cfc22fef0b9a484b82a78d97ee3c9c50bcab
SABBIR <absabbir1@gmail.com>
2025-02-14 19:43:32 +0600

    Advertisement Model Correction

--- app/Models/Advertisements.php
+++ app/Models/Advertisements.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Advertisement extends Model
+class Advertisements extends Model
 {
-    use HasFactory;
-
-    protected $fillable = ['priority', 'content', 'advertiser_name', 'advertiser_contact', 'start_date', 'end_date', 'placement', 'image', 'video', 'link', 'status'];
+    protected $fillable = [
+        'priority',
+        'content',
+        'advertiser_name',
+        'advertiser_contact',
+        'start_date',
+        'end_date',
+        'placement',
+        'image',
+        'video',
+        'link',
+        'status'
+    ];
 }
